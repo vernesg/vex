e0b09369f579482bc9fbf82ceaf97293bf3315a8
kierex <heroxhonattackz20@gmail.com>
2025-05-13 12:58:50 +0800

    Update admins.js

--- script/commands/admins.js
+++ script/commands/admins.js
@@ -2,7 +2,7 @@ module.exports.config = {
     name: 'admins',
     version: '1.0.0',
     permission: 0,
-    credits: 'ryuko',
+    credits: 'vrax',
     prefix: false,
     premium: false,
     description: 'group administrator list.',
