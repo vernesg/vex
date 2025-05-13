6ebba9f05f108092c784d045225d3d212af584fa
kierex <heroxhonattackz20@gmail.com>
2025-05-13 12:54:24 +0800

    Update weather.js

--- script/commands/weather.js
+++ script/commands/weather.js
@@ -3,9 +3,9 @@ module.exports.config = {
 	version: "1.0.1",
 	permission: 0,
 	credits: "ryuko",
-    prefix: false,
-    premium: false,
-	description: "see weather information in the area",
+	prefix: false,
+	premium: false,
+	description: "See weather information in the area",
 	category: "without prefix",
 	usages: "[location]",
 	cooldowns: 5,
@@ -19,39 +19,63 @@ module.exports.config = {
 };
 
 module.exports.languages = {
-
 	"english": {
-		"locationNotExist": "can't find %1.",
-		"returnResult": "temp : %1℃\nfeels like : %2℃\nsky : %3\nhumidity : %4%\nwind speed : %5km/h\nsun rises : %6\nsun sets : %7"
+		"locationNotExist": "Can't find %1.",
+		"returnResult": "Temp: %1℃\nFeels like: %2℃\nSky: %3\nHumidity: %4%%\nWind speed: %5 km/h\nSun rises: %6\nSun sets: %7"
 	},
-    "bangla": {
-		"locationNotExist": "can't find %1.",
-		"returnResult": "temp : %1℃\nfeels like : %2℃\nsky : %3\nhumidity : %4%\nwind speed : %5km/h\nsun rises : %6\nsun sets : %7"
+	"bangla": {
+		"locationNotExist": "%1 খুঁজে পাওয়া যায়নি।",
+		"returnResult": "তাপমাত্রা: %1℃\nঅনুভূত তাপমাত্রা: %2℃\nআকাশ: %3\nআর্দ্রতা: %4%%\nবাতাসের গতি: %5 কিমি/ঘণ্টা\nসূর্যোদয়: %6\nসূর্যাস্ত: %7"
 	}
-}
+};
 
 module.exports.run = async ({ api, event, args, getText }) => {
 	const request = global.nodemodule["request"];
 	const moment = global.nodemodule["moment-timezone"];
 	const { throwError } = global.utils;
 	const { threadID, messageID } = event;
-  const { weather } = global.envConfig;
-	
-	var city = args.join(" ");
-	if (city.length == 0) return throwError(this.config.name, threadID, messageID);
-	return request(encodeURI(`https://api.openweathermap.org/data/2.5/weather?q=` + city + "&appid=" + global.configModule[this.config.name].OPEN_WEATHER + "&units=metric&lang=" + global.config.language), (err, response, body) => {
-		if (err) throw err;
-		var weatherData = JSON.parse(body);
-		if (weatherData.cod !== 200) return api.sendMessage(getText("locationNotExist", city), threadID, messageID);
-		var sunrise_date = moment.unix(weatherData.sys.sunrise).tz("Asia/Manila");
-		var sunset_date = moment.unix(weatherData.sys.sunset).tz("Asia/Manila");
+	const apiKey = global.configModule[this.config.name].OPEN_WEATHER;
+
+	const city = args.join(" ");
+	if (!city) return throwError(this.config.name, threadID, messageID);
+
+	const url = `https://api.openweathermap.org/data/2.5/weather?q=${encodeURIComponent(city)}&appid=${apiKey}&units=metric&lang=${global.config.language}`;
+
+	request(url, (err, response, body) => {
+		if (err) return api.sendMessage("An error occurred while fetching the weather.", threadID, messageID);
+
+		let weatherData;
+		try {
+			weatherData = JSON.parse(body);
+		} catch (e) {
+			return api.sendMessage("Failed to parse weather data.", threadID, messageID);
+		}
+
+		if (weatherData.cod !== 200) {
+			return api.sendMessage(getText("locationNotExist", city), threadID, messageID);
+		}
+
+		const sunrise = moment.unix(weatherData.sys.sunrise).tz("Asia/Manila").format('HH:mm:ss');
+		const sunset = moment.unix(weatherData.sys.sunset).tz("Asia/Manila").format('HH:mm:ss');
+
+		const msg = getText(
+			"returnResult",
+			weatherData.main.temp,
+			weatherData.main.feels_like,
+			weatherData.weather[0].description,
+			weatherData.main.humidity,
+			weatherData.wind.speed,
+			sunrise,
+			sunset
+		);
+
 		api.sendMessage({
-			body: getText("returnResult", weatherData.main.temp, weatherData.main.feels_like, weatherData.weather[0].description, weatherData.main.humidity, weatherData.wind.speed, sunrise_date.format('HH:mm:ss'), sunset_date.format('HH:mm:ss')),
+			body: msg,
 			location: {
 				latitude: weatherData.coord.lat,
 				longitude: weatherData.coord.lon,
 				current: true
-			},
+			}
 		}, threadID, messageID);
 	});
-}
+};
