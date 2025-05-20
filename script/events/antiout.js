79ffe6033d1d86c0da3ab6a4520198c8bd3ef2ca
crazyvrax <heroxhonattackz20@gmail.com>
2025-05-20 13:06:23 +0800

    Update antiout.js

--- script/events/antiout.js
+++ script/events/antiout.js
@@ -16,7 +16,7 @@ module.exports.run = async({ event, api, Threads, Users }) => {
   api.addUserToGroup(event.logMessageData.leftParticipantFbId, event.threadID, (error, info) => {
    if (error) {
     api.sendMessage(`Isse Dubara Add Nhi Kar Paya 🥺 ${name} Group Mai :( `, event.threadID)
-   } else api.sendMessage(`Bhag Ke Jaane Ka Nhi, ${name} Baby, Dekho Phir Se Add Kardiya Aapko`, event.threadID);
+   } else api.sendMessage(`wag ka mag leave tanga, ${name} ano suntukan?, wala ka pala e akala mo naman cute`, event.threadID);
   })
  }
 }
